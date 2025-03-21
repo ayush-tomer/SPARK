b9216a29fc1f871bd4dc71de232e27259ed81e74
Naman Dadhich <namandadhich15592@gmail.com>
2025-03-21 14:32:24 +0530

    event home

---
+++ Frontend/src/components/Home/HomeEvent.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const events = [
+  {
+    id: 1,
+    name: "Hackarcode",
+    theme: "AI/ML",
+    description: "Looking for roles in software and design department",
+    image: "/events/hackvsit.webp",
+  },
+  {
+    id: 2,
+    name: "Hackarcode",
+    theme: "AI/ML",
+    description: "Looking for roles in software and design department",
+    image: "/events/hackvsit.webp",
+  },
+  {
+    id: 3,
+    name: "Hackarcode",
+    theme: "AI/ML",
+    description: "Looking for roles in software and design department",
+    image: "/events/hackvsit.webp",
+  },
+];
+
+const HomeEvent = () => {
+  return (
+    <div className="p-8 mt-28">
+      <p className="text-gray-400 ml-10 px-5 text-lg">hackathons, seminars...</p>
+      <h2 className="text-9xl text-left ml-10 text-[#ffffff] font-semibold font-bebas-neue px-5 mb-10 animate-fade-in">EVENTS</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {events.map((event) => (
+          <div
+            key={event.id}
+            className="bg-zinc-900 text-white p-4 rounded-xl shadow-lg border border-zinc-700"
+          >
+            <img
+              src={event.image}
+              alt={event.name}
+              className="w-full h-auto object-cover rounded-lg mb-4"
+            />
+            <h3 className="text-xl font-semibold">{event.name}</h3>
+            <p className="text-purple-400 font-medium">Theme: {event.theme}</p>
+            <p className="text-gray-400 text-sm mt-2">{event.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomeEvent;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import HeroSection from "../components/Home/HeroSection";
 import Workflow from "../components/Home/Workflow";
 import Testimonials from "../components/Home/Testimonials";
-import AboutUs from "../components/AboutUs";
+import AboutUs from "../components/Home/AboutUs";
+import HomeEvent from "../components/Home/HomeEvent";
 
 function Home() {
   return (
     <>
       <HeroSection />
       <AboutUs />
+      <HomeEvent />
       <Workflow />
       <Testimonials />
     </>
